306c88c9e5b8ea06b3b58830790fad8917eac177
Adam Peaston <adam.peaston@seerdata.com.au>
2021-09-29 12:27:05 +1000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='seer-python-sdk',
+    version='0.1',
+    author="Adam Peaston",
+    author_email="adam.peaston@seerdata.com.au",
+    description="Seer python SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/seer-data/seer-python-sdk.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Private :: Seer public API",
+        "Operating System :: OS Independent",
+    ],
+)
